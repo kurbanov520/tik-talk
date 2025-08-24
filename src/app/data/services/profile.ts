6952ada7d28e65eb7d551d86dd5178f3e48640ec
kurbanov520 <60260060+kurbanov520@users.noreply.github.com>
2025-08-24 15:06:57 +0300

    sidebar

--- src/app/data/services/profile.ts
+++ src/app/data/services/profile.ts
@@ -1,6 +1,8 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {IProfile} from '../interfaces/profile.interface';
+import {map, tap} from 'rxjs';
+import {IPageble} from '../interfaces/pageble.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +13,28 @@ export class Profile {
 
   baseApiUrl = 'https://icherniakov.ru/yt-course/'
 
+  me = signal<IProfile | null>(null)
+
   getTestAccounts() {
     return this.http.get<IProfile[]>(`${this.baseApiUrl}account/test_accounts`)
   }
 
   getMe() {
     return this.http.get<IProfile>(`${this.baseApiUrl}account/me`)
+      .pipe(
+        tap(res => this.me.set(res))
+      )
   }
-}
+
+  getSubscribersShortList() {
+    return this.http.get<IPageble<IProfile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(
+        map(res => res.items.slice(0, 3))
+      )
+  }
+
+  // patchProfile(profile: Partial<IProfile>) {
+  //   return this.http.patch(`${this.baseApiUrl}account/me`, profile)
+  // }
+
+ }
